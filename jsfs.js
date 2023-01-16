946c8f399a1a0268758509f1df85d0052c795884
A Dev <82761748+ADev531@users.noreply.github.com>
2023-01-16 19:11:19 +0900

    Update jsfs.js

--- jsfs.js
+++ jsfs.js
@@ -1,6 +1,6 @@
-fetch('./data.json')
-	    .then((response) => response.json())
-		.then((json) => console.log(json));
+fetch('https://adev531.github.io/HTMLDOS/data.json')
+	 .then((response) => response.json())
+	.then((json) => console.log(json));
 
 function readDisk() {
 	fetch('./data.json')
