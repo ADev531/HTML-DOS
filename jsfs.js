bd4d879fc8d5ecdc1e3cb4ebb3beee95d5008f8e
A Dev <82761748+ADev531@users.noreply.github.com>
2023-04-13 08:27:23 +0900

    Update jsfs.js

--- jsfs.js
+++ jsfs.js
@@ -5,15 +5,6 @@ function saveDisk() {
 	localStorage.setItem("disk", JSON.stringify(disk));
 }
 
-function Format() {
-	//Formats disk
-	fetch('https://adev531.github.io/HTMLDOS/disk.json')
-			.then((response) => response.json())
-    			.then((json) => disk = json)
-			//fetchs default disk.
-			saveDisk();
-}
-
 function readDisk() {
 	try {
 		if (localStorage.getItem("disk") === null) {
@@ -26,7 +17,7 @@ function readDisk() {
 			disk = JSON.parse(localStorage.getItem("disk"));
 		}
 	} catch {
-		iosoutput.innerHTML += "<br>JSFS Error : Disk may Damaged. Please run command 'restore' to restore HTML-DOS."
+		
 	}
 }
 
